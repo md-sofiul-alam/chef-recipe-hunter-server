e0a8aba92373f4482616ba26c02e9024049c990c
md-sofiul-alam <sowrobeakter@gmail.com>
2023-05-11 21:18:27 +0600

    server routes are created

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const recipes = require('./data/recipes.json')
+const blogs = require('./data/blogs.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send("Chef Stories Server");
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes)
+})
+
+app.get('/recipes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const recipe = recipes.find(recipe => recipe.id === id) || {}
+    res.send(recipe);
+})
+
+app.get('/blogs', (req, res) => {
+    res.send(blogs)
+})
+
+app.listen(port, () => {
+    console.log(`Chef Stories Server port ${port}`)
+})
